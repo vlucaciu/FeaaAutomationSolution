5659ab083609920c81a9f94261fd450a60edc22c
Chirila <george.chirila@centric.eu>
2020-04-28 14:34:26 +0300

    Initial commit

---
+++ AutomationSolution/AutomationSolution/LoginTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationSolution
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
+            driver.FindElement(By.Id("sign-in")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.CssSelector("input[data-test='email']")).SendKeys("asd@asd.asd");
+            driver.FindElement(By.CssSelector("input[type='password']")).SendKeys("asd");
+            driver.FindElement(By.XPath("//input[@value='Sign in']")).Click();
+
+            Assert.AreEqual("asd@asd.asd",
+                driver.FindElement(By.CssSelector("span[data-test='current-user']")).Text);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ AutomationSolution/AutomationSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationSolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationSolution")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6f41794-eab0-4833-8102-db1d8cf5a9ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
